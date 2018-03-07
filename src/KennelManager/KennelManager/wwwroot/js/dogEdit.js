4daf9fd0094fb70eeff636c09aae32260cd1e4b5
Nic B <n1scby@yahoo.com>
2018-03-06 21:29:45 -0500

    playing with colors

--- src/KennelManager/KennelManager/wwwroot/js/dogEdit.js
+++ src/KennelManager/KennelManager/wwwroot/js/dogEdit.js
@@ -5,13 +5,14 @@
     var colorCount = document.getElementById("color-count");
 
     var colorTemplate = '<select class="form-control" id="ThisDog.Colors[{{id}}]" name="ThisDog.Colors[{{id}}]">' +
-        '< option value= "" > --select--</option >' +
+        ' <option value=""> --select-- </option>' +
         ' <option value="White">White</option>' +
         ' <option value="Black">Black</option>' +
         ' <option value="Tan">Tan</option> ' +
         ' <option value="Brown">Brown</option>' +
         ' <option value="Gray">Gray</option> ' +
         ' </select >';
+     //   '<button type="button" id="remove-color-btn-{{id}}" class="btn"> - </button>';
 
 
     addColorBtn.addEventListener("click", function () {
@@ -20,14 +21,34 @@
             colorCount.value = 0;
         }
 
+        var newColorButton = document.createElement("button");
+        newColorButton.type = "button";
+        newColorButton.innerText = " - ";
+        newColorButton.classList.add("btn");
+        newColorButton.id = "remove-color-" + colorCount.value;
+        newColorButton.addEventListener("click", function () {
+            removeColor();
+        });
+
         var newColorDiv = document.createElement("div");
         newColorDiv.classList.add("form-group");
 
         newColorDiv.innerHTML = colorTemplate.replace(new RegExp("{{id}}", 'g'), colorCount.value++);
 
+       
+
         colorSection.appendChild(newColorDiv);
+        colorSection.appendChild(newColorButton);
 
 
     });
 
+
+    var removeColor = function removeColor() {
+        alert("remove color, dude");
+    }
+
+  
+
 })();
+
