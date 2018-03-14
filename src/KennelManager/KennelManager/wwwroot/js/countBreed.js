4e408b25531c1c15677ac743bc658f2faad63651
Nic B <n1scby@yahoo.com>
2018-03-14 18:47:34 -0400

    barchart

---
+++ src/KennelManager/KennelManager/wwwroot/js/countBreed.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+(function () {
+    var breedList = [];
+    var breedCount = [];
+    
+    var parentElement = document.getElementById("breed-data");
+
+    var breedData = parentElement.children;
+
+    for (var i = 0; i < breedData.length; i++) {
+        breedList.push(breedData[i].id);
+        breedCount.push(breedData[i].value);
+    }
+        
+    var breedCountData = [
+        {
+            x: breedList,
+            y: breedCount,
+            type: 'bar'
+        }
+    ]
+    
+
+  
+    Plotly.newPlot('graph-breed-count', breedCountData);
+
+})();
+
