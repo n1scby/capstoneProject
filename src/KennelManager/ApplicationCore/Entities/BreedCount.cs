92c0ba07ffe431934c3c16acbb47669ef749de43
Nic B <n1scby@yahoo.com>
2018-03-14 12:34:20 -0400

    working on breed count

---
+++ src/KennelManager/ApplicationCore/Entities/BreedCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class BreedCount
+    {
+        public string BreedName { get; set; }
+        public int Count { get; set; }
+
+    }
+}
--- src/KennelManager/KennelManager/Controllers/HomeController.cs
+++ src/KennelManager/KennelManager/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using KennelManager.Models;
 using ApplicationCore.Interfaces;
 using Microsoft.Extensions.Logging;
+using ApplicationCore.Entities;
 
 namespace KennelManager.Controllers
 {
@@ -39,6 +40,51 @@ namespace KennelManager.Controllers
 
             return View();
         }
+
+        public IActionResult DogsByBreed()
+        {
+            List<BreedCount> breedCountList = new List<BreedCount>();
+            List<Dog> currentDogList = _dogRepo.GetDogList("Adopted");
+
+            foreach(Dog dog in currentDogList)
+            {
+                if (dog.PrimaryBreed == dog.SecondaryBreed)
+                {
+                    dog.SecondaryBreed = "";  //prevent from counting the breed twice
+                }
+            }
+
+            // group by primary breed and get count
+            var countList = currentDogList.GroupBy(i => i.PrimaryBreed);
+            foreach (var grp in countList)
+            {
+                if (grp.Key != "")
+                {
+                    BreedCount bc = new BreedCount();
+                    bc.BreedName = grp.Key;
+                    bc.Count = grp.Count();
+                    breedCountList.Add(bc);
+                }
+            }
+
+            // group by secondary breed and get count
+            var countList2 = currentDogList.GroupBy(i => i.SecondaryBreed);
+            foreach (var grp2 in countList2)
+            {
+                if (grp2.Key != "")
+                {
+                    BreedCount bc = new BreedCount();
+                    bc.BreedName = grp2.Key;
+                    bc.Count = grp2.Count();
+                    breedCountList.Add(bc);
+                }
+
+            }
+
+            return View(breedCountList);
+        }
+
+
 
         public IActionResult Error()
         {
